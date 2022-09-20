bcab126db55df05ced13f2503b1b656d78642005
Morountodun1 <todunadekoya@gmail.com>
2022-09-20 02:23:12 +0100

    :hammer: add program to check sign of number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+#include <stdio.h>
 
 /**
- * Main- Entry point
- * Description: Program that determines if number is positive or negative
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: (int) 0 for success
+ * @author Morountodun Adekoya
  */
 
-int Main(void)
+/* betty style doc for function main goes there */
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	/* your code goes there */
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
