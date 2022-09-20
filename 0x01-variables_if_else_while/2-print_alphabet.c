7031914c4ac1bb737cf7ec09d0bb8d07d0ff31ad
Morountodun1 <todunadekoya@gmail.com>
2022-09-20 13:57:51 -0700

    Program that prnts alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	/* start from a */
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
