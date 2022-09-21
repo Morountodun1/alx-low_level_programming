03c143d486cca274304e0fe9d168a34ea8b59044
Morountodun1 <todunadekoya@gmail.com>
2022-09-21 21:33:13 +0100

    added program to print z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: (0) Sucess
+ * @author Morountodun Adekoya
+ */
+
+int main(void)
+{
+	int lowercase_value = 122;
+
+	while ((lowercase_value > 96) && (lowercase_value < 123))
+	{
+		if ((lowercase_value != 101) && (lowercase_value != 113))
+		{
+			putchar(lowercase_value);
+		}
+		lowercase_value--;
+	}
+	putchar('\n');
+
+	return (0);
+}
