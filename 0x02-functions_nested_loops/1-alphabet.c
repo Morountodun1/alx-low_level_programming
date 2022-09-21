c88f01381b7a88b15c7bf6f0840d4b4248fbd45b
Morountodun1 <todunadekoya@gmail.com>
2022-09-22 00:50:24 +0100

    added function to print lowercase letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet - prints lowercase alphabet
@@ -10,5 +10,12 @@
  * @author Morountodun Adekoya
  */
 void print_alphabet(void)
+{
+	int lowercase_value;
 
-#endif /* MAIN_H */
+	for (lowercase_value = 97; lowercase_value < 123; lowercase_value++)
+	{
+		_putchar(lowercase_value);
+	}
+	_putchar('\n');
+}
