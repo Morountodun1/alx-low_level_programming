94254dfd9bf81de8c902d6fff985c2f7bf78bcd2
Morountodun1 <todunadekoya@gmail.com>
2022-09-22 02:40:00 +0100

    added function to check the sign of integer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -54,4 +54,17 @@ int _islower(int n);
  */
 int _isalpha(int n);
 
+/**
+ * print_sign - check for sign of int
+ * @n: (int) the integer to be checked.
+ *
+ * @brief A function that checks for the sign of an
+ *     integer.
+ * Return: (int) 1 if is +, 0 if is zero and
+ *     -1 if is -
+ * @file 5-sign.c
+ * @author Morountodun Adekoya
+ */
+int print_sign(int n);
+
 #endif /* MAIN_H */
